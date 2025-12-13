b65c7c4131b36da17fbad265cc0f22b338eb921e
Rishav Kashyap <rishavkashyap7120@gmail.com>
2025-12-13 20:05:13 +0530

    add Level and Score  routes

--- backend/routes/levels.js
+++ backend/routes/levels.js
@@ -1,6 +1,35 @@
 import express from 'express';
 const router = express.Router();
+const Level = await import('../models/Level.js');
 
 
+router.get('/', async (req, res) => {
+    try {
+        const levels = await Level.find().sort({ createdAt: -1 });
+        res.json(levels);
+    }
+    catch (error) {
+        res.status(500).json({ message: 'Server Error' });
+    }
+});
+
+router.post('/', async (req, res) => {
+    const level = new Level({
+        name: req.body.name,
+        description: req.body.description,
+        creator: req.body.creator,
+        maze: req.body.maze,
+        k: req.body.k,
+        par: req.body.par
+    });
+
+    try {
+        const newLevel = await level.save();
+        res.status(201).json(newLevel);
+    }
+    catch (error) {
+        res.status(400).json({ message: 'Bad Request' });
+    }
+});
 
 export default router;
--- backend/routes/scores.js
+++ backend/routes/scores.js
@@ -1,6 +1,39 @@
 import express from 'express';
 const router = express.Router();
+const Score = await import('../models/Score.js');
 
 
+//for getting leaderboard particular level ke leye
+router.get('/:levelId', async (req, res) => {
+    try {
+        const scores = await Score.find({ levelId: req.params.levelId })
+        .sort({ efficiency: 1, time: 1, steps: 1 })
+        .limit(10);
+        res.json(scores);
+    }
+    catch (error) {
+        res.status(500).json({ message: 'Server Error' });
+    }
+});
+
+
+//for posting score
+router.post('/', async (req, res) => {
+    const score = new Score({
+        levelId: req.body.levelId,
+        playerName: req.body.playerName,
+        steps: req.body.steps,
+        time: req.body.time,
+        efficiency: req.body.efficiency
+    });
+
+    try {
+        const newScore = await score.save();
+        res.status(201).json(newScore);
+    }
+    catch (error) {
+        res.status(400).json({ message: 'Bad Request' });
+    }
+});
 
 export default router;
